cb0ad724ecf93a7b997059df79a64065a17294a5
Maxi Vallejos <maxis393@gmail.com>
2016-08-24 21:34:13 -0300

    Serial communication

--- lightsControl.ino
+++ lightsControl.ino
@@ -1,44 +1,102 @@
 
 
-const bool BT_ENABLED = true; //bluetooth
-const bool LD_ENABLED = true; //light sensor
+const bool BT_ENABLED = false; //bluetooth
+const bool LS_ENABLED = false; //light sensor
+const bool COM_ENABLED = true;
 
+const int PIN_LED=9;
+int lightLevel = 0;
+  
 void setup() {
   Serial.begin(9600); //especifica baudrate para la conexion serial
-
+  pinMode(PIN_LED,OUTPUT);
 }
 
 
-void loop() {
- 
-  int lightLevel = 0;
-  char btData = '';
-  
+void loop() { 
+
+
   if(LS_ENABLED)
   {
   }
   if(BT_ENABLED)
   {
+      char btData;
     btData = readFromBT();
   }
-  setLightState(lightState);
+  if(COM_ENABLED)
+  {
+    int data = readFromCom();
+    if(data >= 0)
+    {
+      lightLevel = data;
+    }
+  }
+  Serial.print("\n");
+  Serial.print(lightLevel);
+  setLightLevel(lightLevel);
 
-  
+ 
+   delay(1000);
+}
 
+int readFromCom()
+{
+    if(Serial.available() > 0)      // chequeo si hay data
+   {
+
+      int data = Serial.parseInt();        
+      Serial.print("\n com ");
+      Serial.print(data);          //imprimo en consola serial
+      Serial.print("\n");        
+      
+    //  Serial.write("In byte ");
+  //    int inBytes = unitToByte(data);
+//       Serial.write(parseInt(inBytes));
+       
+      return data;
+   }
+   else
+   {
+     return -1;
+   }
 }
 
 char readFromBT()
 {
   if(Serial.available() > 0)      // chequeo si hay data
    {
-      data = Serial.read();        
+      char data = Serial.read();        
+      Serial.print("\n bt ");
       Serial.print(data);          //imprimo en consola serial
       Serial.print("\n");        
       return data;
    }
+   else
+   {
+     return ' ';
+   }
 }
 
-void setLightState(bool level)
+void setLightLevel(int level)
 {
+  if(lightLevel != level)
+  {
+    Serial.write("\n Level");
+    Serial.write(level);
+    Serial.write("\n");
   //turn on/off  
+   analogWrite(PIN_LED, level);
+   lightLevel = level;
+   
+       
+   
+   
+  }
+  
+}
+
+int unitToByte(int value)
+{
+  return 25 * value;
 }
